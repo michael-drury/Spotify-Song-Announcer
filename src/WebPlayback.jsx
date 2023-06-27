8d3f8d594b7a11e518f429a35f380aa0e7448c93
Michael Drury <michael_drury@hotmail.co.uk>
2023-06-27 20:19:18 +0100

    Major refactoring of .jsx file

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -1,83 +1,54 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { render } from 'react-dom';
+import React, { useState, useEffect, useCallback } from 'react';
 import Logout from './Logout';
 
-window.announcer = false;
-
 function CallOutSong(toBeCalledOut) {
-
-    console.log("Arg " + toBeCalledOut);
     const instanceOfspeech = new SpeechSynthesisUtterance(toBeCalledOut);
     window.speechSynthesis.speak(instanceOfspeech);
 }
 
-function WebPlayback(props) {
+async function getCurrentTrack(accessToken) {
+    const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + accessToken },
+    });
+    return response.json();
+}
 
-    console.log(props.token);
-    if (!window.announcer) {
-        console.log("Starting main");
-        window.announcer = true;
-        main(props.token);
-    }
+function WebPlayback({ token }) {
+    const [isAnnouncerActive, setIsAnnouncerActive] = useState(false);
+
+    const readTrackInfo = useCallback(async () => {
+        const trackInformation = await getCurrentTrack(token);
+        if (!trackInformation) return;
+
+        const { artists, name: song, duration_ms: durationMs} = trackInformation.item;
+        const {name: album} = trackInformation.item.album;
+        const { progress_ms: progressMs } = trackInformation;
+        const artist = artists[0].name;
+
+        const trackData = `Now playing ${song} by ${artist} from ${album}`;
+        console.log(trackData);
+
+        if (progressMs < 5000) {
+            console.log("Calling out song info");
+            CallOutSong(trackData);
+        }
+        else{
+            console.log("Not speaking song name");
+        }
+        
+        const remainingMs = (durationMs - progressMs);
+        setTimeout(readTrackInfo, remainingMs + 1000);
+    }, [token]);
+
+    useEffect(() => {
+        if (!isAnnouncerActive) {
+            setIsAnnouncerActive(true);
+            readTrackInfo();
+        }
+    }, [readTrackInfo, isAnnouncerActive]);
+
     return <Logout />;
 }
 
-async function main(access_tok) {
-
-    fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + access_tok },
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            readTrackInfo(data, access_tok)
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
-}
-
-function readTrackInfo(trackInformation, access_tok) {
-    console.log(trackInformation);
-    if (!trackInformation) return;
-
-    const artist = trackInformation.item.artists[0].name;
-    const song = trackInformation.item.name;
-    const album = trackInformation.item.album.name;
-    const duration = trackInformation.item.duration_ms;
-    const progress = trackInformation.progress_ms;
-
-    /** TODO: Split into different functions  */
-
-    var remaining = (duration - progress);
-    var startTime = new Date();
-
-    console.log("prog: " + progress + " dur: " + duration);
-
-    const trackData = "Now playing " + song + " by " + artist + " from " + album;
-
-    if (!trackData) return;
-    console.log(trackData);
-
-    if (remaining == 0) return;
-
-    /** TODO: Errors during transition when remaining/progress is 0 */
-    if (progress < 5000) {
-        console.log("Speaking");
-        CallOutSong(trackData)
-    }
-    else console.log("Not speaking");
-    var endTime = new Date();
-    var durationSpeaking = endTime - startTime;
-    var nextCheckSong = remaining + 1000 - durationSpeaking
-    console.log("Next api ping in " + nextCheckSong)
-    setTimeout(main, nextCheckSong, access_tok);
-}
-
-export default WebPlayback
-
-
-/** TODO: Refresh tokens. Display text of currently playing + any errors 
- *  TODO: Ping API every min when no song playing
- *  TODO: Secure handling of functions
-*/
+export default WebPlayback;
